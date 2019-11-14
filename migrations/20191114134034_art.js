63bd28c86d418cc4c6754d2d4ec03560eef67c5b
meginks <meganjones.dev@gmail.com>
2019-11-14 13:47:16 -0600

    added art table

--- migrations/20191114134034_art.js
+++ migrations/20191114134034_art.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex) {
-  
-};
+    return knex.schema.createTable('art', table => {
+        table.increments(); // id
+        table.string("title"); 
+        table.string("shortTitle");
+        table.text("description"); 
+        table.date("dateCreated"); 
+})
+}
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('art');
 };
