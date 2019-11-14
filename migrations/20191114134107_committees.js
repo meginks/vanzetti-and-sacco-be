6437809921780db7f8647bdcb46749145b303272
meginks <meganjones.dev@gmail.com>
2019-11-14 13:50:38 -0600

    added committees

--- migrations/20191114134107_committees.js
+++ migrations/20191114134107_committees.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex) {
-  
-};
+    return knex.schema.createTable('committees', table => {
+        table.increments(); // id
+        table.string("name"); 
+        table.text("description"); 
+        table.date("dateIncorporated"); 
+})
+}
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('committees');
 };
