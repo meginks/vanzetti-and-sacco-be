248ddc1593e98b9532ad0906aec14a3a5ffc6216
meginks <meganjones.dev@gmail.com>
2019-11-14 15:39:42 -0600

    added publishers

--- migrations/20191114134217_publishers.js
+++ migrations/20191114134217_publishers.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('publishers', table => {
+        table.increments(); // id
+        table.string("name"); 
+        table.string("city");
+});
 };
+    
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('publishers');
 };
