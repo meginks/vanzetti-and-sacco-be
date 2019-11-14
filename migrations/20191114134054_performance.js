c2f370657da278501a6188601febf74bfcedfc80
meginks <meganjones.dev@gmail.com>
2019-11-14 13:49:45 -0600

    added performances migration

--- migrations/20191114134054_performance.js
+++ migrations/20191114134054_performance.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex) {
-  
-};
+    return knex.schema.createTable('performances', table => {
+        table.increments(); // id
+        table.string("title"); 
+        table.string("venue");
+        table.text("description"); 
+        table.date("datePerformed"); 
+})
+}
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('performances');
 };
