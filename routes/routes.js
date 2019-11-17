03507d873db100989256f79c932736f1e2acbe49
meginks <meganjones.dev@gmail.com>
2019-11-17 15:48:41 -0600

    added post, put and delete routes for the people table

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const People = require('../models/models.js');
 
-router.get('/', (req, res) => {
+router.get('/people', (req, res) => {
     try {
         const people = People.findPeople()
         res.status(200).json({people})
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
     }
 }) 
 
-router.get('/:id', (req, res) => {
+router.get('/person/:id', (req, res) => {
     try {
         const person = People.findPersonById(req.params.id)
         res.status(200).json({person}) 
@@ -21,3 +21,33 @@ router.get('/:id', (req, res) => {
         res.status(500).json({error})
     }
 }) 
+
+router.post('/person', (req, res) => {
+    try {
+        const newPerson = await addPerson(req.body)
+        res.status(200).json(newPerson)
+    }
+    catch (error) {
+        res.status(500).json({error})
+    }
+})
+
+router.put('/person/:id', (req, res) => {
+    try {
+        let updatedPerson = updatePerson(req.params.id, req.body) 
+        res.status(200).json(updatedPerson)
+    }
+    catch (error) {
+        res.status(500).json({error})
+    }
+}) 
+
+router.delete('/person/:id', (req, res) => {
+    try {
+        let deletedPerson = removePerson(req.params.id) 
+        res.status(200).json(deletedPerson)
+    } 
+    catch (error) {
+        res.status(500).json({error})
+    }
+})
